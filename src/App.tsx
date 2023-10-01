c44ceccbf97f78f2a33d2880157def43dbffbabc
kingcrud12 <dipitay@gmail.com>
2023-10-01 14:31:53 +0200

    react init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TheNkniLogo from "./the_nkini_logo_D.png"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-logo">
+        <img src={TheNkniLogo} className="App-logo" alt="logo" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
